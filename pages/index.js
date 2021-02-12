d85475eaf663460739b71fb5cf6bc4a9e27c04dc
Rasha Rahman <rasharahman@rashas-mbp.lan>
2021-02-11 19:20:19 -0800

    okkk

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { gql, useQuery } from "@apollo/client";
 import { useEffect, useRef, useState } from "react";
 
@@ -52,7 +51,7 @@ const useKeypress = (e) => {
   return (
     <div>
       <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-        <Image onClick={() => window.location.reload()} src="/logo.png" width="50px" height="50px" layout="fixed" />
+        <img onClick={() => window.location.reload()} src="/logo.png" width="50px" height="50px" layout="fixed" />
         <h1>Randomizer</h1>
       </div>
       <h3>dev by <span style={{textDecoration: 'underline'}}><a href="https://rasha.world">rasha</a></span> using <span style={{textDecoration: 'underline'}}><a href="https://zora.engineering/subgraph">zora subgraph</a></span></h3>
